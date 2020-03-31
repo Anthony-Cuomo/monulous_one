d621325f2ad45ad03980861aae73202631392475
Anthony <pondhockeygold4@outlook.com>
2020-03-30 22:47:11 -0400

    added counting of bits

--- poly_two.js
+++ poly_two.js
@@ -57,10 +57,43 @@ var zygote = (mc0, mc1, fc0, fc1) => {
 
 var cell_rep = () => {
 
+    var c0_count_zero = 0;
+    var c0_count_one = 0;
+    var c1_count_zero = 0;
+    var c1_count_one = 0;
+
+    //counting instances of 0 and zero this will be turned into a method later
+    for (var i = 0; i < cell.chromosome_0.length; i++) {
+        if (cell.chromosome_0[i] == 1) {
+            c0_count_zero++
+        } else {
+            c0_count_one++
+        }
+
+    }
+
+    for (var i = 0; i < cell.chromosome_0.length; i++) {
+        if (cell.chromosome_1[i] == 1) {
+            c1_count_zero++
+        } else {
+            c1_count_one++
+        }
+
+    }
+
+    //printing counts of chromosome 0
+    console.log("This is the count of 1's in chromosome 0: " + c0_count_one);
+    console.log("This is the count of 0's in chromosome 0: " + c0_count_zero);
+
+    //printing counts of chromosome 1
+    console.log("This is the count of 1's in chromosome 1: " + c1_count_one);
+    console.log("This is the count of 0's in chromosome 1: " + c1_count_zero);
 
 }
 
 zygote(male.chromo_0, male.chromo_1, female.chromo_0, female.chromo_1);
+cell_rep();
 
+//prints out cell after zygote is created
 console.log(cell);
 
